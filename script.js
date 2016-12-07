39d9144d70bdf7a1c703c6e45d0bc6b113ddcbe9
Travis Blanton <Travis.blanton@ymail.com>
2016-12-07 15:14:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
   var temp;
   var html = "";
 
-  $.getJSON("http://ip-api.com/json", function(lldata) {
+  $.getJSON("https://ip-api.com/json", function(lldata) {
     lat = lldata.lat;
     long = lldata.lon;
-    var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=c68bf5c6ba0168f01ae8b1db47a489e6&units=imperial';
+    var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=c68bf5c6ba0168f01ae8b1db47a489e6&units=imperial';
     $.getJSON(url, function(data) {
-      html += "<img src = 'http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' " + "alt='" + data.weather[0].description + "'>";
+      html += "<img src = 'https://openweathermap.org/img/w/" + data.weather[0].icon + ".png' " + "alt='" + data.weather[0].description + "'>";
       tempf = Math.round(data.main.temp) + "°F";
       temp = tempf;
       $("#city").html(data.name);
