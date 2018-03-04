b15dc8782d11de57b8fcb8b79434cb900ae8a4bf
Travis Blanton <Travis.blanton@ymail.com>
2018-03-04 00:51:01 -0600

    Updated Open Weather API
    Upodated to HTTPS for Open Weather API

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
   $.getJSON("https://ipapi.co/json/", function(lldata) {
     lat = lldata.latitude;
     long = lldata.longitude;
-    var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=c68bf5c6ba0168f01ae8b1db47a489e6&units=imperial';
+    var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&APPID=c68bf5c6ba0168f01ae8b1db47a489e6&units=imperial';
     $.getJSON(url, function(data) {
-      html += "<img src = 'http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' " + "alt='" + data.weather[0].description + "'>";
+      html += "<img src = 'https://openweathermap.org/img/w/" + data.weather[0].icon + ".png' " + "alt='" + data.weather[0].description + "'>";
       tempf = Math.round(data.main.temp) + "°F";
       temp = tempf;
       $("#city").html(data.name);
